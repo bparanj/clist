74a70c068455db7d878882216cbdb7e601e72515
Bala Paranj <bparanj@onekingslane.com>
2016-06-28 15:34:15 -0700

    Create a guest account for a user to try the app without creating an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,9 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(allowed_params)
+    @user = params[:user] ? User.new(allowed_params) : User.new_guest
     if @user.save
+      current_user.move_to(@user) if current_user && current_user.guest?
       session[:user_id] = @user.id
       redirect_to root_url
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+  
+  validates_presence_of :handle, :email, :password_digest
+  validates_uniqueness_of :handle
+  validates_confirmation_of :password
   
   has_secure_password
+  
+  def self.new_guest
+    new(email: "#{Time.now}@bogus.com", 
+        handle: Time.now,
+        password_digest: 'bogus')
+  end
+  
+  def move_to(user)
+    tasks.update_all(user_id: user.id)
+  end
+  
+  def guest?
+    email.include?('bogus.com')
+  end
 end
